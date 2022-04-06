bce52d0b794bcebc42da820868dfaaca0c7b06ea
adityash1 <aditya167411@gmail.com>
2022-04-06 20:10:26 +0530

    meme image state setup and css

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,4 +1,15 @@
+import { useState } from 'react';
+import memesData from '../memesdata.js';
+
 export default function Meme() {
+    const [image, setImage] = useState(null);
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setImage(memesArray[randomNumber].url) 
+    }
+
     return (
         <main>
             <div className="form">
@@ -14,9 +25,11 @@ export default function Meme() {
                 />
                 <button 
                     className="form--button"
+                    onClick={getMemeImage}
                 >
                     Get a new meme image 🖼
                 </button>
+                <img src={image} alt="meme" className='meme--image'/>
             </div>
         </main>
     )
